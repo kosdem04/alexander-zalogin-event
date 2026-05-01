fa6d2ed7428977845b930bed016c9ccadac5c696
student-zp-41 <dementiev.kos04@gmail.com>
2026-05-01 09:00:43 +0700

    Добавил progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 
 import AppRouter from "./router/AppRouter";
+import ScrollProgressBar from "./components/ScrollProgressBar";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollProgressBar />
       <AppRouter />
     </BrowserRouter>
   );
