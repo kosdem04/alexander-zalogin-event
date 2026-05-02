db7c83bca9aa3fa78fd0b47fcedb92dda00d5594
student-zp-41 <dementiev.kos04@gmail.com>
2026-05-02 22:18:04 +0700

    Изменил медиа

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -26,7 +26,7 @@ const aboutPoints = [
 
 const verticalVideos = [
   { title: "Свадьба", src: mediaLinks.reelsVideoUrl },
-  { title: "Корпоратив", src: "https://s3.twcstorage.ru/707f08c3-950c-4237-807b-4b7b9b3a486e/%D1%80%D0%B8%D0%BB%D1%81%20%D0%BA%D0%BE%D1%80%D0%BF%D0%BE%D1%80%D0%B0%D1%82%D0%B8%D0%B2.mp4" },
+  { title: "Корпоратив", src: "https://s3.twcstorage.ru/707f08c3-950c-4237-807b-4b7b9b3a486e/%D0%9F%D1%80%D0%BE%D0%BC%D0%BE%20%D1%81%D1%82%D0%B8%D0%BB%D1%8F%D0%B3%D0%B8.mp4" },
   { title: "Юбилей", src: "https://s3.twcstorage.ru/707f08c3-950c-4237-807b-4b7b9b3a486e/%D1%80%D0%B8%D0%BB%D1%81%20%D1%8E%D0%B1%D0%B8%D0%BB%D0%B5%D0%B9.mp4" },
 ];
 
