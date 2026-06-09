f890de4532f95319ec540ebe11fd575fe35c5c07
student-zp-41 <dementiev.kos04@gmail.com>
2026-06-09 22:35:26 +0700

    Добавил в блок "Клиенты и бренды" "Жилфонд" и "Gabbiano"

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -60,7 +60,7 @@ const results = [
   "Спокойствие за ваше мероприятие",
 ];
 
-const clients = ["Лента", "Магнит", "РТС-Тендер", "2ГИС"];
+const clients = ["Лента", "Магнит", "РТС-Тендер", "2ГИС", "Жилфонд", "Gabbiano"];
 
 const reviewPhotos = [
   { src: "/feedbacks/%D0%B4%D0%B2%D0%B0%20%D0%B4%D0%B8%D0%B2%D0%B0%D0%BD%D0%B0.png", alt: "Отзыв с мероприятия: два дивана" },
