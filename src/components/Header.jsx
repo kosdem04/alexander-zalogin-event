4da210fbf28b5d6cc665415814440984a27d6547
student-zp-41 <dementiev.kos04@gmail.com>
2026-09-10 22:19:34 +0700

    Изменил текст кнопки в хэдере

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
         </NavLink>
 
         <a href="/#lead" className={styles.cta}>
-          Узнать свободную дату
+          Запросить дату
         </a>
       </div>
     </header>
